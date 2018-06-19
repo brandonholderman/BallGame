2e89fc2abf974e63c7520bb8b6606f2306c47a5b
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-06-19 07:26:31 -0700

    ball is now interactive and camera follows ball

--- BallGame/GameViewController.swift
+++ BallGame/GameViewController.swift
@@ -26,17 +26,65 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         setupScene()
         setupNodes()
+        setupSounds()
     }
     
     func setupScene() {
         sceneView = self.view as! SCNView
-        sceneView.allowsCameraControl = true
+        sceneView.delegate = self
+        
+//        sceneView.allowsCameraControl = true
         scene = SCNScene(named: "art.scnassets/MainScene.scn")
         sceneView.scene = scene
+        
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.numberOfTapsRequired = 1
+        tapRecognizer.numberOfTouchesRequired = 1
+        
+        tapRecognizer.addTarget(self, action: #selector(GameViewController.sceneViewTapped(recognizer:)))
+        sceneView.addGestureRecognizer(tapRecognizer)
     }
     
     func setupNodes() {
         ballNode = scene.rootNode.childNode(withName: "ball", recursively: true)!
+        selfieStickNode = scene.rootNode.childNode(withName: "selfieStick", recursively: true)!
+    }
+    
+    func setupSounds() {
+        let sawSound = SCNAudioSource(fileNamed: "chainsaw.wav")!
+        let jumpSound = SCNAudioSource(fileNamed: "jump.wav")!
+        sawSound.load()
+        jumpSound.load()
+        sawSound.volume = 0.3
+        jumpSound.volume = 0.4
+        
+        sounds["saw"] = sawSound
+        sounds["jump"] = jumpSound
+        
+        let backgroundMusic = SCNAudioSource(fileNamed: "background.mp3")!
+        backgroundMusic.volume = 0.1
+        backgroundMusic.loops = true
+        backgroundMusic.load()
+        
+        let musicPlayer = SCNAudioPlayer(source: backgroundMusic)
+        ballNode.addAudioPlayer(musicPlayer)
+    }
+    
+    @objc func sceneViewTapped(recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: sceneView)
+        
+        let hitResults = sceneView.hitTest(location, options: nil)
+        
+        if hitResults.count > 0 {
+            let result = hitResults.first
+            if let node = result?.node {
+                if node.name == "ball" {
+                    let jumpSound = sounds["jump"]!
+                    ballNode.runAction(SCNAction.playAudio(jumpSound, waitForCompletion: false))
+                    ballNode.physicsBody?.applyForce(SCNVector3(x: 0, y: 4, z: -2), asImpulse: true)
+                }
+            }
+        }
     }
     
     override var shouldAutorotate: Bool {
@@ -51,5 +99,27 @@ class GameViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+}
 
+extension GameViewController : SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        let ball = ballNode.presentation
+        let ballPosition = ball.position
+        
+        let targetPosition = SCNVector3(x: ballPosition.x, y: ballPosition.y + 5, z: ballPosition.z + 5)
+        var cameraPostion = selfieStickNode.position
+        
+        let cameraDamping:Float = 0.3
+        
+        let xComponent = cameraPostion.x * (1 - cameraDamping) + targetPosition.x * cameraDamping
+        let yComponent = cameraPostion.y * (1 - cameraDamping) + targetPosition.y * cameraDamping
+        let zComponent = cameraPostion.z * (1 - cameraDamping) + targetPosition.z * cameraDamping
+        
+        cameraPostion = SCNVector3(x: xComponent, y: yComponent, z: zComponent)
+        selfieStickNode.position = cameraPostion
+        
+        motion.getAccelerometerData { ( x, y, z) in
+            self.motionForce = SCNVector3(x: x * 0)
+    }
+    
 }
